c73a7f066b6635891aba4111a94f9a13db162838
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-05-16 12:04:14 +0530

    feat: new theme
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+$(document).ready(function(){
+	
+	if($('.parallax').length){
+	    $('.parallax').parallax();
+	}
+});
+  }); // end of document ready
+})(jQuery);
